2401b6ab04fa62563badf5392b8900e5f5fa7dcc
Sharon CR <schullunquia@unsa.edu.pe>
2021-12-01 23:06:14 -0500

    Test events

--- src/index.js
+++ src/index.js
@@ -3,15 +3,28 @@ import sha1 from 'crypto-js/sha1';
 import sha256 from 'crypto-js/sha256';
 
 import { default as md4 } from './md4';
+import { suma } from './suma';
 
-const text = 'Hello';
-const digestMD4 = md4(text);
-const digestMD5 = md5(text);
-const digestSHA1 = sha1(text);
-const digestSHA256 = sha256(text);
+import './index.css';
 
-console.log('Cadena:', text);
-console.log('MD4', digestMD4);
-console.log('MD5', digestMD5.toString());
-console.log('SHA1', digestSHA1.toString());
-console.log('SHA256', digestSHA256.toString());
+const btn = document.querySelector('button');
+const rpta = document.querySelector('#md5');
+
+btn.addEventListener('click', function () {
+  const text = document.querySelector('#texto').value;
+
+  const digestMD4 = md4(text);
+  const digestMD5 = md5(text);
+  const digestSHA1 = sha1(text);
+  const digestSHA256 = sha256(text);
+
+  rpta.value = digestMD5.toString();
+
+  console.log('Cadena:', text);
+  console.log('MD4', digestMD4);
+  console.log('MD5', digestMD5.toString());
+  console.log('SHA1', digestSHA1.toString());
+  console.log('SHA256', digestSHA256.toString());
+});
+
+suma();
---
+++ src/suma.js
@@ -0,0 +1,5 @@
+function suma() {
+  console.log(2 + 3);
+}
+
+export { suma };
