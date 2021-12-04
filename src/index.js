7f62ed142e250fc9275945f9f3cb4933151dba58
Sharon CR <schullunquia@unsa.edu.pe>
2021-12-04 00:07:38 -0500

    Add hmac functions

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import md5 from 'crypto-js/md5';
 import sha1 from 'crypto-js/sha1';
 import sha256 from 'crypto-js/sha256';
+import hmacSHA512 from 'crypto-js/hmac-sha512';
+import hmacMD5 from 'crypto-js/hmac-md5';
+import hmacSHA1 from 'crypto-js/hmac-sha1';
 
 import { default as md4 } from './md4';
 
@@ -26,5 +29,17 @@ btn.addEventListener('click', function () {
     result.value = digestSHA1.toString();
   } else if (seleccion[3].selected) {
     result.value = digestSHA256.toString();
+  } else if (seleccion[4].selected) {
+    const clave = document.querySelector('#key').value;
+    const digesthmacsha512 = hmacSHA512(mensaje, clave);
+    result.value = digesthmacsha512.toString();
+  } else if (seleccion[5].selected) {
+    const clave = document.querySelector('#key').value;
+    const digesthmacMD5 = hmacMD5(mensaje, clave);
+    result.value = digesthmacMD5.toString();
+  } else if (seleccion[6].selected) {
+    const clave = document.querySelector('#key').value;
+    const digesthmacSHA1 = hmacSHA1(mensaje, clave);
+    result.value = digesthmacSHA1.toString();
   }
 });
